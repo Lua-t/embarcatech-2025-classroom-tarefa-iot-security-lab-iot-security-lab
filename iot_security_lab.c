6e5db5bee40a9e6b2132f45e6d7c282e8d61e2b4
Lua-t <lauracarol180@gmail.com>
2025-05-26 21:53:03 -0300

    security

--- iot_security_lab.c
+++ iot_security_lab.c
@@ -1,40 +1,49 @@
-// Bibliotecas necessárias
 #include <string.h>                 // Para funções de string como strlen()
 #include "pico/stdlib.h"            // Biblioteca padrão do Pico (GPIO, tempo, etc.)
 #include "pico/cyw43_arch.h"        // Driver WiFi para Pico W
 #include "include/wifi_conn.h"      // Funções personalizadas de conexão WiFi
 #include "include/mqtt_comm.h"      // Funções personalizadas para MQTT
 #include "include/xor_cipher.h"     // Funções de cifra XOR
+#include <time.h>
+#include <stdint.h>
+
+#define BUTTON_A 5 
 
 int main() {
     // Inicializa todas as interfaces de I/O padrão (USB serial, etc.)
     stdio_init_all();
-    
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
     // Conecta à rede WiFi
     // Parâmetros: Nome da rede (SSID) e senha
-    connect_to_wifi("SSID da rede", "Senha da rede");
+    connect_to_wifi("DUKA", "amizade2014");
 
     // Configura o cliente MQTT
     // Parâmetros: ID do cliente, IP do broker, usuário, senha
-    mqtt_setup("bitdog1", "IP do broker", "aluno", "senha123");
-
+    mqtt_setup("bitdog1", "192.168.1.5", "aluno", "senha123");
     // Mensagem original a ser enviada
     const char *mensagem = "26.5";
-    // Buffer para mensagem criptografada (16 bytes)
-    uint8_t criptografada[16];
-    // Criptografa a mensagem usando XOR com chave 42
-    xor_encrypt((uint8_t *)mensagem, criptografada, strlen(mensagem), 42);
-
+    sleep_ms(3000);
     // Loop principal do programa
+    int n=0;
+
     while (true) {
-        // Publica a mensagem original (não criptografada)
-        mqtt_comm_publish("escola/sala1/temperatura", mensagem, strlen(mensagem));
-        
-        // Alternativa: Publica a mensagem criptografada (atualmente comentada)
-        // mqtt_comm_publish("escola/sala1/temperatura", criptografada, strlen(mensagem));
-        
+        if(gpio_get(BUTTON_A)==0){
+            char buffer[256];
+            sprintf(buffer, "{\"valor\":%s,\"ts\":%lu}", mensagem, n);
+            uint8_t criptografada[256];// Criptografa a mensagem usando XOR com chave 42
+            xor_encrypt((uint8_t *)buffer, criptografada, strlen(buffer), 42); // Publica a mensagem original (não criptografada)
+         // Alternativa: Publica a mensagem criptografada (atualmente comentada)
+            mqtt_comm_publish("escola/sala1/temperatura", criptografada, strlen(buffer));
+            n++;
         // Aguarda 5 segundos antes da próxima publicação
-        sleep_ms(5000);
+            sleep_ms(5000);
+        }
+        else{
+            mqtt_comm_subscribe("escola/sala1/temperatura");
+            sleep_ms(5000);
+        }
     }
     return 0;
 }
--- src/mqtt_comm.c
+++ src/mqtt_comm.c
@@ -2,7 +2,7 @@
 #include "include/mqtt_comm.h"    // Header file com as declarações locais
 // Base: https://github.com/BitDogLab/BitDogLab-C/blob/main/wifi_button_and_led/lwipopts.h
 #include "lwipopts.h"             // Configurações customizadas do lwIP
-
+#include <string.h>
 /* Variável global estática para armazenar a instância do cliente MQTT
  * 'static' limita o escopo deste arquivo */
 static mqtt_client_t *client;
@@ -20,6 +20,48 @@ static void mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection
     }
 }
 
+static int inpub_id;
+static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len)
+{
+  printf("Incoming publish at topic %s with total length %u\n", topic, (unsigned int)tot_len);
+
+  /* Decode topic string into a user defined reference */
+  if(strcmp(topic, "print_payload") == 0) {
+    inpub_id = 0;
+  } else if(topic[0] == 'A') {
+    /* All topics starting with 'A' might be handled at the same way */
+    inpub_id = 1;
+  } else {
+    /* For all other topics */
+    inpub_id = 2;
+  }
+}
+
+static void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags)
+{
+  printf("Incoming publish payload with length %d, flags %u\n", len, (unsigned int)flags);
+
+  if(flags & MQTT_DATA_FLAG_LAST) {
+    /* Last fragment of payload received (or whole part if payload fits receive buffer
+       See MQTT_VAR_HEADER_BUFFER_LEN)  */
+
+    /* Call function or do action depending on reference, in this case inpub_id */
+    if(inpub_id == 0) {
+      /* Don't trust the publisher, check zero termination */
+      if(data[len-1] == 0) {
+        printf("mqtt_incoming_data_cb: %s\n", (const char *)data);
+      }
+    } else if(inpub_id == 1) {
+
+      /* Call an 'A' function... */
+    } else {
+      printf("mqtt_incoming_data_cb: Ignoring payload...\n");
+    }
+  } else {
+
+    /* Handle fragmented payload, store in buffer, write to file or whatever */
+  }
+}
 /* Função para configurar e iniciar a conexão MQTT
  * Parâmetros:
  *   - client_id: identificador único para este cliente
@@ -48,6 +90,8 @@ void mqtt_setup(const char *client_id, const char *broker_ip, const char *user, 
         .client_user = user,     // Usuário (opcional)
         .client_pass = pass      // Senha (opcional)
     };
+    mqtt_set_inpub_callback(client, mqtt_incoming_publish_cb,mqtt_incoming_data_cb, NULL );
+
 
     // Inicia a conexão com o broker
     // Parâmetros:
@@ -59,12 +103,20 @@ void mqtt_setup(const char *client_id, const char *broker_ip, const char *user, 
     //   - &ci: informações de conexão
     mqtt_client_connect(client, &broker_addr, 1883, mqtt_connection_cb, NULL, &ci);
 }
-
 /* Callback de confirmação de publicação
  * Chamado quando o broker confirma recebimento da mensagem (para QoS > 0)
  * Parâmetros:
  *   - arg: argumento opcional
  *   - result: código de resultado da operação */
+
+ static void mqtt_sub_request_cb(void *arg, err_t result){
+        if (result == ERR_OK) {
+        printf("Inscricao MQTT enviada com sucesso!\n");
+    } else {
+        printf("Erro ao receber via MQTT: %d\n", result);
+    }
+ }
+
 static void mqtt_pub_request_cb(void *arg, err_t result) {
     if (result == ERR_OK) {
         printf("Publicação MQTT enviada com sucesso!\n");
@@ -78,6 +130,18 @@ static void mqtt_pub_request_cb(void *arg, err_t result) {
  *   - topic: nome do tópico (ex: "sensor/temperatura")
  *   - data: payload da mensagem (bytes)
  *   - len: tamanho do payload */
+void mqtt_comm_subscribe(const char *topic) {
+    // Envia a mensagem MQTT
+    err_t status = mqtt_subscribe(
+        client,              // Instância do cliente
+        topic,  
+        0,     
+        mqtt_sub_request_cb, 
+        NULL
+    );
+        if (status != ERR_OK) {
+        printf("mqtt_subscribe falhou ao receber: %d\n", status);
+    }}
 void mqtt_comm_publish(const char *topic, const uint8_t *data, size_t len) {
     // Envia a mensagem MQTT
     err_t status = mqtt_publish(
@@ -95,3 +159,4 @@ void mqtt_comm_publish(const char *topic, const uint8_t *data, size_t len) {
         printf("mqtt_publish falhou ao ser enviada: %d\n", status);
     }
 }
+
